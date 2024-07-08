57d899fc34def419ec06f09e99513ba6132e7ad9
lieonlion <lieonlion4@gmail.com>
2024-07-08 18:43:58 +0100

    1.0.4

--- src/main/java/io/github/lieonlion/lolmbv/MoreBookshelfVariants.java
+++ src/main/java/io/github/lieonlion/lolmbv/MoreBookshelfVariants.java
@@ -3,8 +3,9 @@ package io.github.lieonlion.lolmbv;
 import io.github.lieonlion.lolmbv.init.MbvItemInit;
 import io.github.lieonlion.lolmbv.init.MbvBlockInit;
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.FireBlock;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.FireBlock;
 
 public class MoreBookshelfVariants implements ModInitializer {
     public static final String MODID = "lolmbv";
@@ -19,13 +20,17 @@ public class MoreBookshelfVariants implements ModInitializer {
 
     private void registerFlammableBlocks() {
         FireBlock fireBlock = (FireBlock) Blocks.FIRE;
-        fireBlock.registerFlammableBlock(MbvBlockInit.SPRUCE_BOOKSHELF, 30, 20);
-        fireBlock.registerFlammableBlock(MbvBlockInit.BIRCH_BOOKSHELF, 30, 20);
-        fireBlock.registerFlammableBlock(MbvBlockInit.JUNGLE_BOOKSHELF, 30, 20);
-        fireBlock.registerFlammableBlock(MbvBlockInit.ACACIA_BOOKSHELF, 30, 20);
-        fireBlock.registerFlammableBlock(MbvBlockInit.DARK_OAK_BOOKSHELF, 30, 20);
-        fireBlock.registerFlammableBlock(MbvBlockInit.MANGROVE_BOOKSHELF, 30, 20);
-        fireBlock.registerFlammableBlock(MbvBlockInit.CHERRY_BOOKSHELF, 30, 20);
-        fireBlock.registerFlammableBlock(MbvBlockInit.BAMBOO_BOOKSHELF, 30, 20);
+        fireBlock.setFlammable(MbvBlockInit.SPRUCE_BOOKSHELF, 30, 20);
+        fireBlock.setFlammable(MbvBlockInit.BIRCH_BOOKSHELF, 30, 20);
+        fireBlock.setFlammable(MbvBlockInit.JUNGLE_BOOKSHELF, 30, 20);
+        fireBlock.setFlammable(MbvBlockInit.ACACIA_BOOKSHELF, 30, 20);
+        fireBlock.setFlammable(MbvBlockInit.DARK_OAK_BOOKSHELF, 30, 20);
+        fireBlock.setFlammable(MbvBlockInit.MANGROVE_BOOKSHELF, 30, 20);
+        fireBlock.setFlammable(MbvBlockInit.CHERRY_BOOKSHELF, 30, 20);
+        fireBlock.setFlammable(MbvBlockInit.BAMBOO_BOOKSHELF, 30, 20);
+    }
+
+    public static ResourceLocation asId(String path) {
+        return new ResourceLocation(MODID, path);
     }
 }
