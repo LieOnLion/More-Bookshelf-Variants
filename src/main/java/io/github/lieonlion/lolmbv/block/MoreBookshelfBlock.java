e4865c7ef9ba2c0f6620ea36d902aa7d8628977e
lieonlion <lieonlion4@gmail.com>
2024-07-08 18:58:55 +0100

    1.0.4 (block folder)

--- src/main/java/io/github/lieonlion/lolmbv/block/MoreBookshelfBlock.java
+++ src/main/java/io/github/lieonlion/lolmbv/block/MoreBookshelfBlock.java
@@ -1,9 +1,8 @@
-package io.github.lieonlion.lolmbv.bock;
+package io.github.lieonlion.lolmbv.block;
 
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
 import net.minecraft.world.level.material.MapColor;
 
 public class MoreBookshelfBlock extends Block {
